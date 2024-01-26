4dfe7d50a7185eed50ec830b3bc7a7f74a0defbf
mehakraina2108 <124191847+mehakraina2108@users.noreply.github.com>
2024-01-26 16:22:37 +0530

    Kanpsack_Problem.cpp

--- Kanpsack_Problem.cpp
+++ Kanpsack_Problem.cpp
@@ -3,21 +3,26 @@ using namespace std;
 
 //Function to solve using DP table
 int knapsack(vector<int>& weight, vector<int>& values, int n, int givenlimit) {
+    //Create a dp table
     vector<vector<int>> dp(n, vector<int>(givenlimit + 1, 0));
-
+    //base case
     for (int i = weight[0]; i <= givenlimit; i++) {
         dp[0][i] = values[0];
     }
+    
+    //bottom-up approach
 
     for (int i = 1; i < n; i++) {
         for (int c = 0; c <= givenlimit; c++) {
+            //not take the current item
             int ntk = dp[i - 1][c];
+            //take the current item
             int tk = INT_MIN;
 
             if (weight[i] <= c) {
                 tk = values[i] + dp[i - 1][c - weight[i]];
             }
-
+            //max value from taking and not taking
             dp[i][c] = max(ntk, tk);
         }
     }
